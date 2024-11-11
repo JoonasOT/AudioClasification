8e3bb1051ab1e1202d7187fb99ec365cdf1a4502
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-11-11 18:21:38 +0200

    commenting

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from src.functions.plotting import plotSignal, plotTransfrom, keepPlotsOpen
 def main():
     [
         Maybe(file)
-        .transform(getAudioSignal)
-        .run(AudioSignal.play)
-        .transform(fft)
-        .run(plotSignal)
-        .run(plotTransfrom)
-        .transform(amplitudeToDB)
-        .run(plotTransfrom)
+        .transform(getAudioSignal)  # Get the actual audio
+        # .run(AudioSignal.play)    # Play the recording
+        .transform(fft)             # Get the DFT of the signal
+        .run(plotSignal)            # Plot the signal in the time domain
+        # .run(plotTransfrom)       # Plot the signal in the frequency domain
+        .transform(amplitudeToDB)   # Transfrom the DFT values to DB scale
+        .run(plotTransfrom)         # Plot the signal in the frequency domain
         for file in ["./data/audio1.wav", "./data/audio2.wav", "./data/x.wav"]
     ]
     keepPlotsOpen()
