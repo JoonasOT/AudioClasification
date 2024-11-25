67240e543fe71fa85b3347ac49e98c612c2695ad
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-11-26 00:20:48 +0200

    Better import handling

--- src/dependencies/dependencies.py
+++ src/dependencies/dependencies.py
@@ -1,4 +1,11 @@
+from sys import stderr
 try:
     from FunctionalMaybe import FunctionalMaybe as Maybe
 except ModuleNotFoundError as E:
-    from .FunctionalMaybe import FunctionalMaybe as Maybe
+    try:
+        from .FunctionalMaybe import FunctionalMaybe as Maybe
+        print(f"The package functional-maybe is not installed. Using the submodule!", file=stderr)
+    except ModuleNotFoundError as e:
+        raise Exception("Project doesn't contain the FunctionalMaybe-module."
+                        "Either clone the submodules of this repository or install "
+                        "package \"functional-maybe\" with pip.")
