3f5567c66839a6560d46d8e5115227d8dcc6e874
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-11-26 06:30:15 +0200

    Nearest neighbor implemented

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from src.functions.audio_manipulation import FreqType, getNormalizedAudio, sampl
 from src.functions.plotting import keepPlotsOpen
 
 
-DIRECTORY: Final[str] = "./data"
+DATA_DIR: Final[str] = "./data"
+TEST_DIR: Final[str] = "/test"
+TRAIN_DIR: Final[str] = "/train"
 
 SAMPLERATE: Final[int] = 22100
 N_SAMPLES: Final[int] = int(2.0 * SAMPLERATE)
@@ -20,26 +22,48 @@ WIN_SIZE: Final[float] = 0.032
 HOP_SIZE: Final[float] = WIN_SIZE / 2
 N_MFCC: Final[int] = 40
 
-MFCCs = []
+MFCCs = {}
 
 
 def main():
-    for file in onlyWavFiles(getFilesInDir("./data")):
-
+    for file in onlyWavFiles(getFilesInDir(DATA_DIR + TRAIN_DIR)):
+        print(file)
         # Create a normalized AudioSignal
-        audio = getNormalizedAudio(file, plot=False).transformers(sampleTo(SAMPLERATE))
+        audio = getNormalizedAudio(file, plot=False)
 
         # Form spectrums
-        spectrum = getSpectrum(audio, FreqType.DECIBEL, plot=False)
+        # spectrum = getSpectrum(audio, FreqType.DECIBEL, plot=False)
 
         # Form spectrograms
-        spectrogram = getSpectrogram(audio, FreqType.DECIBEL, WIN_SIZE, HOP_SIZE, plot=False)
+        # spectrogram = getSpectrogram(audio, FreqType.DECIBEL, WIN_SIZE, HOP_SIZE, plot=False)
 
         mfcc = getMFCC(audio, N_MFCC, WIN_SIZE, HOP_SIZE, plot=False)
 
-        MFCCs.append(np.mean(mfcc.unwrap().T, axis=0))
+        model = file.split("/")[-2]
+        if model in MFCCs:
+            MFCCs[model].append(np.mean(mfcc.unwrap().T, axis=0))
+        else:
+            MFCCs[model] = [np.mean(mfcc.unwrap().T, axis=0)]
 
-    print(MFCCs)
+    for file in onlyWavFiles(getFilesInDir(DATA_DIR + TEST_DIR)):
+        print(file)
+        # Create a normalized AudioSignal
+        audio = getNormalizedAudio(file, plot=False)
+
+        # Form spectrums
+        # spectrum = getSpectrum(audio, FreqType.DECIBEL, plot=False)
+
+        # Form spectrograms
+        # spectrogram = getSpectrogram(audio, FreqType.DECIBEL, WIN_SIZE, HOP_SIZE, plot=False)
+
+        mfcc = getMFCC(audio, N_MFCC, WIN_SIZE, HOP_SIZE, plot=False)
+
+        for m, mfccs in MFCCs.items():
+            dists = []
+            for mfcc_ in mfccs:
+                dists.append(np.linalg.norm(np.mean(mfcc.unwrap().T, axis=0) - mfcc_))
+            dists = np.sort(dists)
+            print(f"For {file} the matches for {m} where:\n{dists}")
 
     keepPlotsOpen()
 
