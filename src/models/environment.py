c8684f1eb68a8ef9b0a64323989316af125d72c7
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-12-07 03:08:47 +0200

    rant

--- src/models/environment.py
+++ src/models/environment.py
@@ -1,3 +1,14 @@
 import os
+
+# Set couple flags to configure Tensorflow and Keras
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+# Without the last flag importing said modules will always print
+# a warning printout, so this is the way to suppress that
+
+# Why in a separate file? Excelent questiom
+# Since python linters want import statements to be the first lines,
+# setting these flags will always cause linters to have a stroke as
+# they have to be set before importing the modules...
+
+# So this is a dumb workararound for that :)
