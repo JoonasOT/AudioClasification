a33295921a912365b670f0adf26d2804f3b81165
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-12-14 17:36:16 +0200

    Update visualizations

--- visuals.py
+++ visuals.py
@@ -1,21 +1,60 @@
-from main import SAMPLERATE, N_SAMPLES, WIN_SIZE, HOP_SIZE
-from src.functions.audio_manipulation import getNormalizedAudio, sampleTo, getSpectrum, getSpectrogram, FreqType, \
-    limitSamplesTo
+import matplotlib.pyplot as plt
+from random import shuffle
+
+# Get settings
+from main import SAMPLERATE, N_SAMPLES, WIN_SIZE, HOP_SIZE, N_MFCC
+# Get file management functions and plotting convenience functions
 from src.functions.file_management import onlyWavFiles, getFilesInDir
-from src.functions.plotting import keepPlotsOpen
+from src.functions.plotting import keepPlotsOpen, save
+
+# Functions for getting and transforming audio
+from src.functions.audio_manipulation import getNormalizedAudio, limitSamplesTo, sampleTo
+# Functions for getting fft, and stft of an audio
+from src.functions.audio_manipulation import FreqType, getSpectrum, getSpectrogram
+# Function for getting mfcc
+from src.functions.audio_manipulation import getMFCC
+
+
+def getImages(dir_: str) -> dict[str, list[str]]:
+    tmp = {file: file.split("/")[-2] for file in onlyWavFiles(getFilesInDir(dir_))}
+    images: dict[str, list[str]] = {label: [] for _, label in tmp.items()}
+    for file, label in tmp.items():
+        images[label].append(file)
+    return images
+
+
+SAVE_DIR = "./img/"
+TAKE = 10
 
 
 def viz():
-    for file in onlyWavFiles(getFilesInDir("./data")):
-        # Create a normalized AudioSignal
-        audio = getNormalizedAudio(file, plot=True).transformers(sampleTo(SAMPLERATE), limitSamplesTo(N_SAMPLES))
+    for label, files in getImages("./data/").items():
+        print(f"Label: {label}")
+        shuffle(files)
+        for index, file in enumerate(files[:TAKE]):
+            # Create a normalized AudioSignal
+            plot = False
+            audio = getNormalizedAudio(file, plot=plot).transformers(sampleTo(SAMPLERATE), limitSamplesTo(N_SAMPLES))
+            if plot:
+                save(f"audio_{label}_{index}")
 
-        # Form spectrums
-        spectrum = getSpectrum(audio, FreqType.DECIBEL, plot=True)
+            # Form spectrums
+            plot = False
+            spectrum = getSpectrum(audio, FreqType.DECIBEL, plot=plot)
+            if plot:
+                save(f"spectrum_{label}_{index}")
 
-        # Form spectrograms
-        spectrogram = getSpectrogram(audio, FreqType.DECIBEL, WIN_SIZE, HOP_SIZE, plot=True)
-        break
+            # Form spectrograms
+            plot = True
+            spectrogram = getSpectrogram(audio, FreqType.DECIBEL, WIN_SIZE, HOP_SIZE, plot=plot)
+            if plot:
+                save(f"spectrogram_{label}_{index}")
+
+            # Form MFCCs
+            plot = False
+            mfcc = getMFCC(audio, N_MFCC, WIN_SIZE, HOP_SIZE, plot=plot)
+            if plot:
+                save(f"mfcc_{label}_{index}")
 
     keepPlotsOpen()
 
